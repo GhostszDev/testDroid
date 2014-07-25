6c5ea86b7311de3581e6800a9251ef9911554121
Steven Rivera <ghostszmusic@gmail.com>
2014-07-25 05:22:54 -0400

    more movement

--- Assets/testDroid/scripts/touchControls.cs
+++ Assets/testDroid/scripts/touchControls.cs
@@ -8,14 +8,23 @@ public class touchControls : MonoBehaviour {
 
 	public GameObject yetti;
 
+	bool left;
+	bool right;
+
 	float h;
 	float v;
-
 	float speed = 0.2f;
+
+	int ro = 0;
 
 
 	// Use this for initialization
 	void Start () {
+
+		left = true;
+		right = false;
+
+
 	
 	}
 	
@@ -26,14 +35,39 @@ public class touchControls : MonoBehaviour {
 			
 			if(touch.phase == TouchPhase.Stationary && leftBtn.HitTest(touch.position)){
 
-				h = speed;
+				h = -speed;
+
+				if(ro == 180){
+					left = true;
+				}
+
+				if (left){
+					yetti.transform.Rotate(0,180,0);
+					left = false;
+				}
 				
+			}
+
+			else if(touch.phase == TouchPhase.Ended){
+
+				h = 0.0f;
 			}
 
 			if(touch.phase == TouchPhase.Stationary && rightBtn.HitTest(touch.position)){
 				
-				h = -speed;
+				h = speed;
+				right = true;
+
+				if(right){
+					yetti.transform.Rotate(0,270,0);
+					right = false;
+				}
 				
+			}
+
+			else if(touch.phase == TouchPhase.Ended){
+
+				h = 0.0f;
 			}
 
 		}
